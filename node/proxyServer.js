74a3697f1a3fe812f27496b5db82e2b501301784
Brijesh Singh <bsingh6636@gmail.com>
2024-06-13 23:24:04 +0530

    c

--- node/proxyServer.js
+++ node/proxyServer.js
@@ -1,10 +1,10 @@
+// proxyServer.js
 const express = require('express');
 const request = require('request');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Middleware to handle CORS and preflight requests
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
@@ -15,22 +15,11 @@ app.use((req, res, next) => {
     next();
 });
 
-// Route to respond with "Hello World" on the root URL
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-// Proxy endpoint for all other routes
-app.use('/proxy', (req, res) => {
-    const url = req.url.substring(1); // Remove leading '/'
-    if (!url) {
-        return res.status(400).send('Bad Request: URL is missing');
-    }
-
+app.use('/', (req, res) => {
+    const url = req.url.substring(1);
     req.pipe(request(url)).pipe(res);
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Proxy server running on port ${PORT}`);
 });
