5debbfe0f52c9e30349ef0c0ac6721a0cf119609
Brijesh Singh <bsingh6636@gmail.com>
2024-06-13 14:18:58 +0530

    c

--- node/proxyserver.js
+++ node/proxyserver.js
@@ -10,8 +10,7 @@ app.use(morgan('combined'));
 
 // Middleware to handle CORS and preflight requests
 app.use((req, res, next) => {
-    // res.header('Access-Control-Allow-Origin', 'https://travel-planner-weather-dashboard.vercel.app/'); // Change to your frontend's URL in production
-    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     if (req.method === 'OPTIONS') {
@@ -20,6 +19,7 @@ app.use((req, res, next) => {
     next();
 });
 
+// Root route to print a message on the screen
 app.get('/', (req, res) => {
     res.send('Welcome to the Proxy Server!');
 });
@@ -31,13 +31,17 @@ app.use('/proxy', (req, res) => {
         return res.status(400).send('Bad Request: URL is missing');
     }
 
-
     req.pipe(request(url, (error, response, body) => {
         if (error) {
             console.error('Request error:', error);
             return res.status(500).send('Internal Server Error');
         }
     })).pipe(res);
+});
+
+// 404 handler for unknown routes
+app.use((req, res, next) => {
+    res.status(404).send('404: Not Found');
 });
 
 // Error handling middleware
